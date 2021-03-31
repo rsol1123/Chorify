7cfba0420f10e5c8508450d37f423836bba9cc05
Rupesh <rsol1123@gmail.com>
2021-03-30 20:50:00 -0500

    Delete Chorify-master directory

--- Chorify-master/chorify/android/app/src/main/kotlin/com/example/chorify/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.chorify
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Chorify-master/chorify/lib/login/Testing.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Testing extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
